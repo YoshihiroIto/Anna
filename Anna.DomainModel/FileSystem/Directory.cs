3b847d1510b939071fd10d2258c301b92ccf3912
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-05 19:25:17 +0900

    Add FileSystem model class

---
+++ Anna.DomainModel/FileSystem/Directory.cs
@@ -0,0 +1,43 @@
+﻿using Anna.DomainModel.Foundations;
+using System.Collections.ObjectModel;
+
+namespace Anna.DomainModel.FileSystem;
+
+public class Directory : NotificationObject
+{
+    #region Path
+
+    private string _Path = "";
+
+    public string Path
+    {
+        get => _Path;
+        private set => SetProperty(ref _Path, value);
+    }
+
+    #endregion
+    
+    #region Directories
+
+    private ObservableCollection<FileObject> _Directories = new();
+
+    public ObservableCollection<FileObject> Directories
+    {
+        get => _Directories;
+        private set => SetProperty(ref _Directories, value);
+    }
+
+    #endregion
+
+    #region Files
+
+    private ObservableCollection<FileObject> _Files = new();
+
+    public ObservableCollection<FileObject> Files
+    {
+        get => _Files;
+        private set => SetProperty(ref _Files, value);
+    }
+
+    #endregion
+}
---
+++ Anna.DomainModel/FileSystem/FileObject.cs
@@ -0,0 +1,18 @@
+﻿using Anna.DomainModel.Foundations;
+
+namespace Anna.DomainModel.FileSystem;
+
+public class FileObject : NotificationObject
+{
+    #region Name
+
+    private string _Name = "";
+
+    public string Name
+    {
+        get => _Name;
+        private set => SetProperty(ref _Name, value);
+    }
+
+    #endregion
+}

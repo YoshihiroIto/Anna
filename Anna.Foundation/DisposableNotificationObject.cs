5e3b92758a1d73373fc3ac66789c58f82fc40f43
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-11 14:03:14 +0900

    Add DisposableNotificationObject

---
+++ Anna.Foundation/DisposableNotificationObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive.Disposables;
+
+namespace Anna.Foundation;
+
+public class DisposableNotificationObject : NotificationObject, IDisposable
+{
+    protected CompositeDisposable Trash =>
+        LazyInitializer.EnsureInitialized(ref _trashes, () => new CompositeDisposable()) ??
+        throw new NullReferenceException();
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing)
+            _trashes?.Dispose();
+
+        _disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private CompositeDisposable? _trashes;
+
+    private bool _disposed;
+}
--- Anna.Gui/Foundations/ViewModelBase.cs
+++ Anna.Gui/Foundations/ViewModelBase.cs
@@ -1,45 +1,15 @@
 ﻿using Anna.DomainModel.Interface;
 using Anna.Foundation;
-using System;
 using System.Reactive.Disposables;
-using System.Threading;
 
 namespace Anna.Foundations;
 
-public class ViewModelBase : NotificationObject, IDisposable
+public class ViewModelBase : DisposableNotificationObject
 {
-    protected CompositeDisposable Trash =>
-        LazyInitializer.EnsureInitialized(ref _trashes, () => new CompositeDisposable()) ??
-        throw new NullReferenceException();
-
     protected ViewModelBase(IObjectLifetimeChecker objectLifetimeChecker)
     {
-        _objectLifetimeChecker = objectLifetimeChecker;
-
-        _objectLifetimeChecker.Add(this);
+        objectLifetimeChecker.Add(this);
+        
+        Trash.Add(Disposable.Create(objectLifetimeChecker, c => c.Remove(this)));
     }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (_disposed)
-            return;
-
-        if (disposing)
-            _trashes?.Dispose();
-
-        _disposed = true;
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-
-        _objectLifetimeChecker.Remove(this);
-    }
-
-    private CompositeDisposable? _trashes;
-
-    private bool _disposed;
-    private readonly IObjectLifetimeChecker _objectLifetimeChecker;
 }

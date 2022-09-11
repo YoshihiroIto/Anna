c58807876cd5369f9bfd13fb0c396031c7a2203c
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-11 15:37:34 +0900

    Add ObservableFileSystemWatcher

--- Anna.DomainModel/FileSystem/FileSystemDirectory.cs
+++ Anna.DomainModel/FileSystem/FileSystemDirectory.cs
@@ -1,14 +1,25 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Anna.DomainModel.Interface;
+using Anna.Foundation;
+using Reactive.Bindings.Extensions;
+using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Anna.DomainModel.Operator")]
 
 namespace Anna.DomainModel.FileSystem;
 
-public sealed class FileSystemDirectory : Directory
+public sealed class FileSystemDirectory : Directory, IDisposable
 {
-    internal FileSystemDirectory(string path)
+    private readonly IObjectLifetimeChecker _objectLifetimeChecker;
+
+    internal FileSystemDirectory(string path, IObjectLifetimeChecker objectLifetimeChecker)
         : base(path)
     {
+        _objectLifetimeChecker = objectLifetimeChecker;
+        _objectLifetimeChecker.Add(this);
+
+        SetupWatcher(path);
     }
 
     protected override void Update()
@@ -29,4 +40,42 @@ public sealed class FileSystemDirectory : Directory
             );
         }
     }
+
+    public void Dispose()
+    {
+        _objectLifetimeChecker.Remove(this);
+        _trash.Dispose();
+    }
+
+    private void SetupWatcher(string path)
+    {
+        var watcher = new ObservableFileSystemWatcher(path).AddTo(_trash);
+
+        watcher.Created
+            .Subscribe(
+                x => Debug.WriteLine("Created:" + x.FullPath)
+            ).AddTo(_trash);
+
+        watcher.Changed
+            .Subscribe(
+                x => Debug.WriteLine("Changed:" + x.FullPath)
+            ).AddTo(_trash);
+
+        watcher.Deleted
+            .Subscribe(
+                x => Debug.WriteLine("Deleted:" + x.FullPath)
+            ).AddTo(_trash);
+
+        watcher.Renamed
+            .Subscribe(
+                x => Debug.WriteLine("Renamed:" + x.FullPath)
+            ).AddTo(_trash);
+
+        watcher.Errors
+            .Subscribe(
+                x => Debug.WriteLine("Errors:" + x.GetException())
+            ).AddTo(_trash);
+    }
+
+    private readonly CompositeDisposable _trash = new();
 }
---
+++ Anna.Foundation/ObservableFileSystemWatcher.cs
@@ -0,0 +1,67 @@
+﻿using System.Reactive.Linq;
+
+namespace Anna.Foundation;
+
+public class ObservableFileSystemWatcher : IDisposable
+{
+    public readonly IObservable<FileSystemEventArgs> Created;
+    public readonly IObservable<FileSystemEventArgs> Changed;
+    public readonly IObservable<FileSystemEventArgs> Deleted;
+    public readonly IObservable<RenamedEventArgs> Renamed;
+    public readonly IObservable<ErrorEventArgs> Errors;
+
+    public ObservableFileSystemWatcher(string path)
+    {
+        _watcher = new FileSystemWatcher(path)
+        {
+            NotifyFilter =
+                NotifyFilters.FileName |
+                NotifyFilters.DirectoryName |
+                NotifyFilters.Attributes |
+                NotifyFilters.Size |
+                NotifyFilters.LastWrite |
+                NotifyFilters.LastAccess |
+                NotifyFilters.CreationTime |
+                NotifyFilters.Security
+        };
+
+        Created = Observable
+            .FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+            h => _watcher.Created += h,
+            h => _watcher.Created -= h)
+            .Select(x => x.EventArgs);
+
+        Changed = Observable
+            .FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+            h => _watcher.Changed += h,
+            h => _watcher.Changed -= h)
+            .Select(x => x.EventArgs);
+
+        Deleted = Observable
+            .FromEventPattern<FileSystemEventHandler, FileSystemEventArgs>(
+            h => _watcher.Deleted += h,
+            h => _watcher.Deleted -= h)
+            .Select(x => x.EventArgs);
+
+        Renamed = Observable
+            .FromEventPattern<RenamedEventHandler, RenamedEventArgs>(
+            h => _watcher.Renamed += h,
+            h => _watcher.Renamed -= h)
+            .Select(x => x.EventArgs);
+
+        Errors = Observable
+            .FromEventPattern<ErrorEventHandler, ErrorEventArgs>(
+            h => _watcher.Error += h,
+            h => _watcher.Error -= h)
+            .Select(x => x.EventArgs);
+
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    public void Dispose()
+    {
+        _watcher.Dispose();
+    }
+
+    private readonly FileSystemWatcher _watcher;
+}

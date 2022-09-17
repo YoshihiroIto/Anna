31f5e64457f2c0a24a3894acdb0226c6d54f72df
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-17 09:22:43 +0900

    Implementation entry list style (temporary)

--- Anna.Gui/ViewModels/EntryViewModel.cs
+++ Anna.Gui/ViewModels/EntryViewModel.cs
@@ -9,13 +9,16 @@ namespace Anna.ViewModels
 {
     public class EntryViewModel : ViewModelBase
     {
+        public string NameWithExtension => _model?.NameWithExtension ?? throw new NullReferenceException();
         public string Name => _model?.Name ?? throw new NullReferenceException();
         public string Extension => _model?.Extension ?? throw new NullReferenceException();
         
+        public bool IsDirectory => _model?.IsDirectory ?? throw new NullReferenceException();
+
         public ReactivePropertySlim<bool> IsSelected { get; }
 
         private Entry? _model;
-        
+
         public EntryViewModel(IObjectLifetimeChecker objectLifetimeChecker)
             : base(objectLifetimeChecker)
         {
@@ -26,6 +29,8 @@ namespace Anna.ViewModels
         {
             _model = model;
             
+            RaisePropertyChanged(nameof(IsDirectory));
+
             return this;
         }
     }

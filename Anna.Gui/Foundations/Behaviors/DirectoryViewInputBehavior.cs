5c331a845f2e7fc23f6ccf3ce9dc6e7eb9c26d2a
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-18 14:29:12 +0900

    Add DirectoryViewInputBehavior

---
+++ Anna.Gui/Foundations/Behaviors/DirectoryViewInputBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Anna.Views;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+
+namespace Anna.Foundations.Behaviors;
+
+public class DirectoryViewInputBehavior : Behavior<DirectoryView>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        if (AssociatedObject is null)
+            return;
+
+        AssociatedObject.Focusable = true;
+        AssociatedObject.AddHandler(InputElement.KeyDownEvent, OnPreviewKeyDown, RoutingStrategies.Tunnel);
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject?.RemoveHandler(InputElement.KeyDownEvent, OnPreviewKeyDown);
+
+        base.OnDetaching();
+    }
+
+    private void OnPreviewKeyDown(object? sender, KeyEventArgs e)
+    {
+    }
+}

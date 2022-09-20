8bb7777f25769b522cbb9ad125850ace69ddc862
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-20 22:09:43 +0900

    Fixed shortcut keys not working after closing menu

--- Anna.Gui/Views/Behaviors/DirectoryViewInputBehavior.cs
+++ Anna.Gui/Views/Behaviors/DirectoryViewInputBehavior.cs
@@ -1,5 +1,7 @@
 ﻿using Anna.Gui.ViewModels;
 using Anna.Gui.ViewModels.ShortcutKey;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Xaml.Interactivity;
@@ -14,27 +16,51 @@ public class DirectoryViewInputBehavior : Behavior<DirectoryView>
     {
         base.OnAttached();
 
-        if (AssociatedObject is null)
-            return;
+        if (AssociatedObject is not null)
+        {
+            AssociatedObject.AttachedToVisualTree += AssociatedObjectOnAttachedToVisualTree;
+            AssociatedObject.Focusable = true;
+        }
+    }
+    private void AssociatedObjectOnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        var parent = AssociatedObject?.Parent;
 
-        AssociatedObject.Focusable = true;
-        AssociatedObject.AddHandler(InputElement.KeyDownEvent, OnPreviewKeyDown, RoutingStrategies.Tunnel);
+        while (parent is not null)
+        {
+            if (parent is Window parentWindow)
+            {
+                _parentWindow = parentWindow;
+                break;
+            }
+
+            parent = parent.Parent;
+        }
+
+        _parentWindow?.AddHandler(InputElement.KeyDownEvent, OnPreviewKeyDown, RoutingStrategies.Tunnel);
     }
 
     protected override void OnDetaching()
     {
-        AssociatedObject?.RemoveHandler(InputElement.KeyDownEvent, OnPreviewKeyDown);
+        if (AssociatedObject is not null)
+            AssociatedObject.AttachedToVisualTree -= AssociatedObjectOnAttachedToVisualTree;
+
+        _parentWindow?.RemoveHandler(InputElement.KeyDownEvent, OnPreviewKeyDown);
 
         base.OnDetaching();
     }
 
     private ValueTask OnPreviewKeyDown(object? sender, KeyEventArgs e)
     {
+        if (FocusManager.Instance?.Current is MenuItem)
+            return ValueTask.CompletedTask;
+
         var manager = _shortcutKeyManager ??=
             (AssociatedObject?.DataContext as DirectoryViewViewModel)?.ShortcutKeyManager;
 
         return manager?.OnKeyDown(AssociatedObject ?? throw new NullReferenceException(), e) ?? ValueTask.CompletedTask;
     }
 
+    private Window? _parentWindow;
     private ShortcutKeyManager? _shortcutKeyManager;
 }

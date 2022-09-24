28784b3738a80aec97caf9a1042ed185e875b079
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-25 08:06:18 +0900

    Add CompositeDisposableExtensions

---
+++ Anna.Foundation/CompositeDisposableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reactive.Disposables;
+
+namespace Anna.Foundation;
+
+public static class CompositeDisposableExtensions
+{
+    public static void Add(this CompositeDisposable c, Action action)
+    {
+        c.Add(Disposable.Create(action));
+    }
+}

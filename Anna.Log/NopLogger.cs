56ddc9da357322150b8d99bdcebcc84d286d7de2
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-27 08:49:36 +0900

    Add NopLogger

---
+++ Anna.Log/NopLogger.cs
@@ -0,0 +1,14 @@
+﻿using Anna.UseCase;
+
+namespace Anna.Log;
+
+public class NopLogger : ILoggerUseCase
+{
+    public void Destroy() {}
+    public void Verbose(string message) {}
+    public void Debug(string message) {}
+    public void Information(string message) {}
+    public void Warning(string message) {}
+    public void Error(string message) { }
+    public void Fatal(string message) { }
+}

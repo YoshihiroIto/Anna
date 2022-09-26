07baa262de2e81fd7d3e14f676b8aa3922063d22
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-26 20:15:19 +0900

    Add InfoPanelViewModel

---
+++ Anna.Gui/Views/Panels/InfoPanelViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Anna.Gui.Foundations;
+using Anna.Gui.Interfaces;
+using Anna.Strings;
+using Anna.UseCase;
+
+namespace Anna.Gui.Views.Panels;
+
+public class InfoPanelViewModel : ViewModelBase, ILocalizableViewModel
+{
+    public Resources R => _resourcesHolder.Instance;
+
+    protected InfoPanelViewModel(
+        ResourcesHolder resourcesHolder,
+        IObjectLifetimeCheckerUseCase objectLifetimeChecker) : base(objectLifetimeChecker)
+    {
+        _resourcesHolder = resourcesHolder;
+    }
+
+    private readonly ResourcesHolder _resourcesHolder;
+}

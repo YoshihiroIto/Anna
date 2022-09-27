cacee375f0baf4e0c1d6ea5a4065f4b9d61abf25
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-09-28 07:11:07 +0900

    Update FileSystemDirectoryTests

--- Anna.DomainModel.Tests/FileSystemDirectoryTests.cs
+++ Anna.DomainModel.Tests/FileSystemDirectoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Anna.TestFoundation;
+﻿using Anna.Constants;
+using Anna.TestFoundation;
 using Xunit;
 
 namespace Anna.DomainModel.Tests;
@@ -6,15 +7,179 @@ namespace Anna.DomainModel.Tests;
 public class FileSystemDirectoryTests : IDisposable
 {
     private readonly TestServiceProviderContainer _dic = new();
-    
-    [Fact]
-    public void Start_and_finish_successfully()
-    {
-         using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(".");
-    }
-    
+    private readonly TempDir _tempDir = new();
+
     public void Dispose()
     {
         _dic.Dispose();
+    }
+
+    [Fact]
+    public void Start_and_finish_successfully()
+    {
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(".");
+    }
+
+    [Fact]
+    public void Initial()
+    {
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        Assert.Equal(_tempDir.RootPath, directory.Path);
+        Assert.Equal(SortModes.Name, directory.SortMode);
+        Assert.Equal(SortOrders.Ascending, directory.SortOrder);
+    }
+
+    [Fact]
+    public async Task Root_directory_does_not_contain_parent_directory_in_entry()
+    {
+        using var directory = _dic.GetInstance<DomainModelOperator>()
+            .CreateDirectory(Path.GetPathRoot(_tempDir.RootPath) ?? throw new NullReferenceException());
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        lock (directory.EntitiesUpdatingLockObj)
+        {
+            Assert.DoesNotContain("..", directory.Entries.Select(x => x.NameWithExtension));
+        }
+    }
+
+    [Fact]
+    public async Task Non_root_directory_contain_parent_directory_in_entries()
+    {
+        var files = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in files)
+            _tempDir.CreateFile(file);
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        var filesWithParentDir = new List<string> { ".." };
+        filesWithParentDir.AddRange(files);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        lock (directory.EntitiesUpdatingLockObj)
+        {
+            Assert.Equal(filesWithParentDir, directory.Entries.Select(x => x.NameWithExtension));
+        }
+    }
+
+    [Fact]
+    public async Task Follow_file_additions()
+    {
+        var filesA = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in filesA)
+            _tempDir.CreateFile(file);
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        var filesB = new[] { "fileD.dat", "fileE.dat", "fileF.dat" };
+
+        foreach (var file in filesB)
+            _tempDir.CreateFile(file);
+
+        var filesWithParentDir = new List<string> { ".." };
+        filesWithParentDir.AddRange(filesA);
+        filesWithParentDir.AddRange(filesB);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        lock (directory.EntitiesUpdatingLockObj)
+        {
+            Assert.Equal(filesWithParentDir, directory.Entries.Select(x => x.NameWithExtension));
+        }
+    }
+
+    [Fact]
+    public async Task Follow_file_deletion()
+    {
+        var filesA = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in filesA)
+            _tempDir.CreateFile(file);
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        File.Delete(Path.Combine(_tempDir.RootPath, "fileB.dat"));
+
+        var filesWithParentDir = new List<string> { ".." };
+        filesWithParentDir.AddRange(filesA);
+        filesWithParentDir.Remove("fileB.dat");
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        lock (directory.EntitiesUpdatingLockObj)
+        {
+            Assert.Equal(filesWithParentDir, directory.Entries.Select(x => x.NameWithExtension));
+        }
+    }
+
+    [Fact]
+    public async Task Follow_file_renaming()
+    {
+        var filesA = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in filesA)
+            _tempDir.CreateFile(file);
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        File.Move(Path.Combine(_tempDir.RootPath, "fileC.dat"), Path.Combine(_tempDir.RootPath, "fileZ.dat"));
+
+        var filesWithParentDir = new List<string> { ".." };
+        filesWithParentDir.AddRange(filesA);
+        filesWithParentDir.Remove("fileC.dat");
+        filesWithParentDir.Add("fileZ.dat");
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        lock (directory.EntitiesUpdatingLockObj)
+        {
+            Assert.Equal(filesWithParentDir, directory.Entries.Select(x => x.NameWithExtension));
+        }
+    }
+
+    [Fact]
+    public async Task IsSelected_is_maintained_even_after_file_renaming()
+    {
+        var filesA = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in filesA)
+            _tempDir.CreateFile(file);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        directory.Entries[3].IsSelected = true;
+
+        File.Move(Path.Combine(_tempDir.RootPath, "fileC.dat"), Path.Combine(_tempDir.RootPath, "fileZ.dat"));
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        Assert.True(directory.Entries[3].IsSelected);
+    }
+
+    [Fact]
+    public async Task IsSelected_is_maintained_even_after_file_overwrite()
+    {
+        var filesA = new[] { "fileA.dat", "fileB.dat", "fileC.dat" };
+
+        foreach (var file in filesA)
+            _tempDir.CreateFile(file);
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        using var directory = _dic.GetInstance<DomainModelOperator>().CreateDirectory(_tempDir.RootPath);
+
+        directory.Entries[3].IsSelected = true;
+
+        await File.WriteAllTextAsync(Path.Combine(_tempDir.RootPath, "fileC.dat"), "overwrite");
+
+        await Task.Delay(TimeSpan.FromMilliseconds(30));
+
+        Assert.True(directory.Entries[3].IsSelected);
     }
 }

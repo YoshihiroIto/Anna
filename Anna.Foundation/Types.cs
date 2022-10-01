9421f2fbeb9af4454f09d63aa202841d1d731f81
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-01 23:40:43 +0900

    IntSize, Implement comparisons

--- Anna.Foundation/Types.cs
+++ Anna.Foundation/Types.cs
@@ -8,6 +8,13 @@ public readonly struct IntSize
         Height = h;
     }
 
+    public static bool operator ==(IntSize x, IntSize y) => x.Width == y.Width && x.Height == y.Height;
+    public static bool operator !=(IntSize x, IntSize y) => x.Width != y.Width || x.Height != y.Height;
+
+    public bool Equals(IntSize other) => Width == other.Width && Height == other.Height;
+    public override bool Equals(object? obj) => obj is IntSize other && Equals(other);
+    public override int GetHashCode() => HashCode.Combine(Width, Height);
+
     public override string ToString()
     {
         return $"({Width}, {Height})";

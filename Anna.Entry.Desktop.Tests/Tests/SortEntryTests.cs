e7f8742895eb25c674a67be2ea2bdcc25aeaa1b7
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-03 07:26:01 +0900

    Add SortEntryTests.Extension_Ascending and SortEntryTests.Extension_Descending

--- Anna.Entry.Desktop.Tests/Tests/SortEntryTests.cs
+++ Anna.Entry.Desktop.Tests/Tests/SortEntryTests.cs
@@ -94,4 +94,67 @@ public class SortEntryTests : IDisposable
         Assert.Equal("002.dat", model.Entries[5].NameWithExtension);
         Assert.Equal("001.dat", model.Entries[6].NameWithExtension);
     }
+    
+    [Fact]
+    public async Task Extension_Ascending()
+    {
+        var configDir = _fixture.ConfigDir;
+        var app = _fixture.App;
+
+        configDir.CreateFile("001.c");
+        configDir.CreateFile("002.b");
+        configDir.CreateFile("003.a");
+
+        var model = await Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            var w = app.DirectoryWindows.First();
+
+            await w.PressKeyAsync(Key.S);
+            await w.PressKeyAsync(Key.Down);
+            await w.PressKeyAsync(Key.Enter);
+            await w.PressKeyAsync(Key.Enter);
+
+            return (w.DataContext as DirectoryWindowViewModel)?.Model;
+        });
+
+        _ = model ?? throw new NullReferenceException();
+
+        Assert.Equal(4, model.Entries.Count);
+        Assert.Equal("..", model.Entries[0].NameWithExtension);
+        Assert.Equal("003.a", model.Entries[1].NameWithExtension);
+        Assert.Equal("002.b", model.Entries[2].NameWithExtension);
+        Assert.Equal("001.c", model.Entries[3].NameWithExtension);
+    }
+    
+    [Fact]
+    public async Task Extension_Descending()
+    {
+        var configDir = _fixture.ConfigDir;
+        var app = _fixture.App;
+
+        configDir.CreateFile("001.c");
+        configDir.CreateFile("002.b");
+        configDir.CreateFile("003.a");
+
+        var model = await Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            var w = app.DirectoryWindows.First();
+
+            await w.PressKeyAsync(Key.S);
+            await w.PressKeyAsync(Key.Down);
+            await w.PressKeyAsync(Key.Enter);
+            await w.PressKeyAsync(Key.Right);
+            await w.PressKeyAsync(Key.Enter);
+
+            return (w.DataContext as DirectoryWindowViewModel)?.Model;
+        });
+
+        _ = model ?? throw new NullReferenceException();
+
+        Assert.Equal(4, model.Entries.Count);
+        Assert.Equal("..", model.Entries[0].NameWithExtension);
+        Assert.Equal("001.c", model.Entries[1].NameWithExtension);
+        Assert.Equal("002.b", model.Entries[2].NameWithExtension);
+        Assert.Equal("003.a", model.Entries[3].NameWithExtension);
+    }
 }

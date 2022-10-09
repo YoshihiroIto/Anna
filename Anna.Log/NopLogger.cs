850a6daa4b9c17ed4a8c4adcc8ab8d0cef34588e
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-10 00:57:53 +0900

    Refactor
    [skip ci]

--- Anna.Log/NopLogger.cs
+++ Anna.Log/NopLogger.cs
@@ -9,6 +9,6 @@ public class NopLogger : ILoggerUseCase
     public void Debug(string message) {}
     public void Information(string message) {}
     public void Warning(string message) {}
-    public void Error(string message) { }
-    public void Fatal(string message) { }
+    public void Error(string message) {}
+    public void Fatal(string message) {}
 }

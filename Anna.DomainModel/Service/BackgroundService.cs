258ee10b90e6e9c9e8f60f1fd9b67db8eb196c35
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-13 08:58:55 +0900

    Add IBackgroundService

---
+++ Anna.DomainModel/Service/BackgroundService.cs
@@ -0,0 +1,84 @@
+﻿using Anna.Foundation;
+using Anna.Service;
+using Anna.Service.Interfaces;
+using System.Threading.Channels;
+
+namespace Anna.DomainModel.Service;
+
+public class BackgroundService : NotificationObject, IBackgroundService, IDisposable
+{
+    #region IsInProcessing
+
+    private bool _IsInProcessing;
+
+    public bool IsInProcessing
+    {
+        get => _IsInProcessing;
+        set => SetProperty(ref _IsInProcessing, value);
+    }
+
+    #endregion
+
+
+    #region ProgressRatio
+
+    private double _ProgressRatio;
+
+    public double ProgressRatio
+    {
+        get => _ProgressRatio;
+        set => SetProperty(ref _ProgressRatio, value);
+    }
+
+    #endregion
+
+
+    #region Message
+
+    private string _Message = "";
+
+    public string Message
+    {
+        get => _Message;
+        set => SetProperty(ref _Message, value);
+    }
+
+    #endregion
+    
+    private Channel<IBackgroundServiceProcess> Channel { get; } =
+        System.Threading.Channels.Channel.CreateUnbounded<IBackgroundServiceProcess>(
+            new UnboundedChannelOptions { SingleReader = true });
+
+    private readonly ManualResetEventSlim _taskCompleted = new();
+
+    public BackgroundService()
+    {
+        Task.Run(async () =>
+        {
+            while (await Channel.Reader.WaitToReadAsync())
+            {
+                if (Channel.Reader.TryRead(out var process))
+                    await process.ExecuteAsync();
+            }
+
+            _taskCompleted.Set();
+        });
+    }
+
+    public void Dispose()
+    {
+        Channel.Writer.TryComplete();
+        _taskCompleted.Wait();
+        _taskCompleted.Dispose();
+    }
+
+    public void CopyFileSystemEntry(string destPath, IEnumerable<IEntry> sourceEntries)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+interface IBackgroundServiceProcess
+{
+    ValueTask ExecuteAsync();
+}
---
+++ Anna.Service/IBackgroundService.cs
@@ -0,0 +1,13 @@
+﻿using Anna.Service.Interfaces;
+using System.ComponentModel;
+
+namespace Anna.Service;
+
+public interface IBackgroundService : INotifyPropertyChanged
+{
+    bool IsInProcessing { get; }
+    double ProgressRatio { get; }
+    string Message { get; }
+
+    void CopyFileSystemEntry(string destPath, IEnumerable<IEntry> sourceEntries);
+}

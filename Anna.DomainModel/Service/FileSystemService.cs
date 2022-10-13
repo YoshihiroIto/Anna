63b2eb3452786b21419e57591bae1e893f21a51c
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-13 20:36:56 +0900

    Change to copy in parallel with FileSystemService.Copy

--- Anna.DomainModel/Service/FileSystemService.cs
+++ Anna.DomainModel/Service/FileSystemService.cs
@@ -30,45 +30,51 @@ public sealed class FileSystemService : IFileSystemService
 
     public void Copy(string destPath, IEnumerable<IEntry> sourceEntries)
     {
-        foreach (var entry in sourceEntries)
-        {
-            var src = entry.Path;
-
-            if (entry.IsFolder)
+        Parallel.ForEach(sourceEntries,
+            entry =>
             {
-                var srcInfo = new DirectoryInfo(src);
-                CopyFolder(srcInfo, destPath);
-            }
-            else
-            {
-                Directory.CreateDirectory(destPath);
+                var src = entry.Path;
 
-                var dest = Path.Combine(destPath, Path.GetFileName(src));
+                if (entry.IsFolder)
+                {
+                    var srcInfo = new DirectoryInfo(src);
+                    CopyFolder(srcInfo, destPath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(destPath);
 
-                File.Copy(src, dest, true);
-                File.SetAttributes(dest, File.GetAttributes(src));
-            }
-        }
+                    var dest = Path.Combine(destPath, Path.GetFileName(src));
+
+                    File.Copy(src, dest, true);
+                    File.SetAttributes(dest, File.GetAttributes(src));
+                }
+            });
     }
 
     private static void CopyFolder(DirectoryInfo srcInfo, string dst)
     {
         var src = srcInfo.FullName;
-        
+
         var targetFolderPath = Path.Combine(dst, Path.GetFileName(src));
         Directory.CreateDirectory(targetFolderPath);
         File.SetAttributes(targetFolderPath, srcInfo.Attributes);
-        
+
         var di = new DirectoryInfo(src);
-        foreach (var file in di.EnumerateFiles())
-        {
-            var dest = Path.Combine(targetFolderPath, file.Name);
 
-            File.Copy(file.FullName, dest, true);
-            File.SetAttributes(dest, file.Attributes);
-        }
+        Parallel.ForEach(di.EnumerateFiles(),
+            file =>
+            {
+                var dest = Path.Combine(targetFolderPath, file.Name);
 
-        foreach (var dir in di.EnumerateDirectories())
-            CopyFolder(dir, targetFolderPath);
+                File.Copy(file.FullName, dest, true);
+                File.SetAttributes(dest, file.Attributes);
+            });
+
+        Parallel.ForEach(di.EnumerateDirectories(),
+            dir =>
+            {
+                CopyFolder(dir, targetFolderPath);
+            });
     }
 }

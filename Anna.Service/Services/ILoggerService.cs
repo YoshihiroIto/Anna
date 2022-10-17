ac91a6c8066fd58adf1ac7b7d506ca8198e3d5d5
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-17 12:54:57 +0900

    Remove LoggerExtensions

--- Anna.Service/Services/ILoggerService.cs
+++ Anna.Service/Services/ILoggerService.cs
@@ -10,11 +10,7 @@ public interface ILoggerService
     void Warning(string message);
     void Error(string message);
     void Fatal(string message);
-}
-
-public static class LoggerExtensions
-{
-    public static void Start(this ILoggerService logger, string name) => logger.Information($"Start [{name}]");
-
-    public static void End(this ILoggerService logger, string name) => logger.Information($"End   [{name}]");
+    
+    public void Start(string name) => Information($"Start [{name}]");
+    public void End(string name) => Information($"End   [{name}]");
 }

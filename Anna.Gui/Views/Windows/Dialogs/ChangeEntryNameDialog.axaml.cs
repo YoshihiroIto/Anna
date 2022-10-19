09edab0d944b09dbc8bc96f435fa4405a73f4530
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-20 05:46:11 +0900

    Fix ChangeEntryNameDialog ViewModel

--- Anna.Gui/Views/Windows/Dialogs/ChangeEntryNameDialog.axaml.cs
+++ Anna.Gui/Views/Windows/Dialogs/ChangeEntryNameDialog.axaml.cs
@@ -4,7 +4,7 @@ using Avalonia.Markup.Xaml;
 
 namespace Anna.Gui.Views.Windows.Dialogs;
 
-public partial class ChangeEntryNameDialog : WindowBase<SelectFolderDialogViewModel>
+public partial class ChangeEntryNameDialog : WindowBase<ChangeEntryNameDialogViewModel>
 {
     public ChangeEntryNameDialog()
     {

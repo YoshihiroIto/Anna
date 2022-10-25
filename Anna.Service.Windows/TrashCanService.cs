3c7baab3562b536a11e94710bffd86e427d80b7a
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-25 20:47:59 +0900

    Changed to use Shell API for Windows.SendToTrashCan implementation

--- Anna.Service.Windows/TrashCanService.cs
+++ Anna.Service.Windows/TrashCanService.cs
@@ -1,26 +1,13 @@
 ﻿using Anna.Foundation;
 using Anna.Service.Interfaces;
 using Anna.Service.Services;
-using Jewelry.Text;
-using System.Buffers;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Exception=System.Exception;
 
 namespace Anna.Service.Windows;
 
 public sealed class TrashCanService : ITrashCanService
 {
     private readonly IServiceProvider _dic;
-    private bool IsInitialized => _sid != "";
-    private string _sid = "";
-    private readonly Random _random = new();
-
-    private static readonly char[] FilenameChars =
-    {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-        'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    };
 
     public TrashCanService(IServiceProvider dic)
     {
@@ -29,26 +16,7 @@ public sealed class TrashCanService : ITrashCanService
 
     public void SendToTrashCan(IEntry target)
     {
-        if (IsInitialized == false)
-            Initialize();
-
-        var metaDataBuffer = ArrayPool<byte>.Shared.Rent(target.Path.Length * 2 + 64);
-        var metaDataBufferSpan = metaDataBuffer.AsSpan();
-
-        try
-        {
-            var trashedPath = MakeTrashedPath(target.Path);
-
-            var size = target.IsFolder ? FileHelper.MeasureFolderSize(new DirectoryInfo(target.Path)) : target.Size;
-            var metaData = MakeMetaData(metaDataBufferSpan, target.Path, size, DateTime.Now);
-
-            Directory.Move(target.Path, trashedPath.Trashed);
-            FileHelper.WriteSpan(trashedPath.MetaData, metaData);
-        }
-        finally
-        {
-            ArrayPool<byte>.Shared.Return(metaDataBuffer);
-        }
+        FileOperationAPIWrapper.Send(target.Path, FileOperationAPIWrapper.FileOperationFlags.FOF_SILENT);
     }
 
     public bool EmptyTrashCan()
@@ -57,10 +25,7 @@ public sealed class TrashCanService : ITrashCanService
         {
             SHEmptyRecycleBin(IntPtr.Zero,
                 null,
-                SHERB_NOCONFIRMATION |
-                SHERB_NOPROGRESSUI |
-                SHERB_NOSOUND
-            );
+                SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
 
             return true;
         }
@@ -71,84 +36,10 @@ public sealed class TrashCanService : ITrashCanService
             return false;
         }
     }
-    
+
     public void OpenTrashCan()
     {
         ProcessHelper.RunAssociatedApp("shell:::{645FF040-5081-101B-9F08-00AA002F954E}");
-    }
-
-    private string FindTrashCanPath(string targetPath)
-    {
-        var driveName = Path.GetPathRoot(targetPath) ?? throw new NullReferenceException();
-
-        return Path.Combine($"{driveName}$Recycle.Bin", _sid);
-    }
-
-    private (string Trashed, string MetaData) MakeTrashedPath(string targetPath)
-    {
-        const string filePrefix = "$R";
-        const string metadataPrefix = "$I";
-
-        var trashCanPath = FindTrashCanPath(targetPath);
-        var extension = Path.GetExtension(targetPath);
-        var name = MakeRandomName();
-
-        return (
-            Path.Combine(trashCanPath, $"{filePrefix}{name}{extension}"),
-            Path.Combine(trashCanPath, $"{metadataPrefix}{name}{extension}")
-        );
-    }
-
-    public string MakeRandomName()
-    {
-        const int fileNameLength = 6;
-
-        return string.Create(
-            fileNameLength,
-            this,
-            (buffer, t) =>
-            {
-                for (var i = 0; i != fileNameLength; ++i)
-                    buffer[i] = FilenameChars[t._random.Next(FilenameChars.Length)];
-            });
-    }
-
-    private void Initialize()
-    {
-        var userInfo = ProcessHelper.ExecuteAndGetStdoutAsync("whoami", "/user").Result;
-
-        using var ss = new StringSplitter(stackalloc StringSplitter.StringSpan[32]);
-
-        var parts = ss.Split(userInfo, ' ', StringSplitOptions.RemoveEmptyEntries);
-        var last = parts[^1];
-
-        _sid = last.ToString(userInfo).TrimEnd();
-    }
-
-    private static ReadOnlySpan<byte> MakeMetaData(
-        Span<byte> buffer, string filePath, long fileSize, DateTime removingDateTime)
-    {
-        var index = 0;
-
-        Unsafe.As<byte, long>(ref buffer[index]) = 2;
-        index += Unsafe.SizeOf<long>();
-
-        Unsafe.As<byte, long>(ref buffer[index]) = fileSize;
-        index += Unsafe.SizeOf<long>();
-
-        Unsafe.As<byte, long>(ref buffer[index]) = removingDateTime.ToFileTime();
-        index += Unsafe.SizeOf<long>();
-
-        Unsafe.As<byte, int>(ref buffer[index]) = filePath.Length + 1;
-        index += Unsafe.SizeOf<int>();
-
-        MemoryMarshal.Cast<char, byte>(filePath.AsSpan()).CopyTo(buffer[index..]);
-        index += filePath.Length * 2;
-
-        Unsafe.As<byte, long>(ref buffer[index]) = 0;
-        index += Unsafe.SizeOf<short>();
-
-        return buffer[..index];
     }
 
     // ReSharper disable all
@@ -158,5 +49,168 @@ public sealed class TrashCanService : ITrashCanService
 
     [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
     private static extern uint SHEmptyRecycleBin(IntPtr hwnd, string? pszRootPath, uint dwFlags);
+
+    // https://stackoverflow.com/questions/3282418/send-a-file-to-the-recycle-bin 
+    private class FileOperationAPIWrapper
+    {
+        /// <summary>
+        /// Possible flags for the SHFileOperation method.
+        /// </summary>
+        [Flags]
+        public enum FileOperationFlags : ushort
+        {
+            /// <summary>
+            /// Do not show a dialog during the process
+            /// </summary>
+            FOF_SILENT = 0x0004,
+
+            /// <summary>
+            /// Do not ask the user to confirm selection
+            /// </summary>
+            FOF_NOCONFIRMATION = 0x0010,
+
+            /// <summary>
+            /// Delete the file to the recycle bin.  (Required flag to send a file to the bin
+            /// </summary>
+            FOF_ALLOWUNDO = 0x0040,
+
+            /// <summary>
+            /// Do not show the names of the files or folders that are being recycled.
+            /// </summary>
+            FOF_SIMPLEPROGRESS = 0x0100,
+
+            /// <summary>
+            /// Surpress errors, if any occur during the process.
+            /// </summary>
+            FOF_NOERRORUI = 0x0400,
+
+            /// <summary>
+            /// Warn if files are too big to fit in the recycle bin and will need
+            /// to be deleted completely.
+            /// </summary>
+            FOF_WANTNUKEWARNING = 0x4000,
+        }
+
+        /// <summary>
+        /// File Operation Function Type for SHFileOperation
+        /// </summary>
+        public enum FileOperationType : uint
+        {
+            /// <summary>
+            /// Move the objects
+            /// </summary>
+            FO_MOVE = 0x0001,
+
+            /// <summary>
+            /// Copy the objects
+            /// </summary>
+            FO_COPY = 0x0002,
+
+            /// <summary>
+            /// Delete (or recycle) the objects
+            /// </summary>
+            FO_DELETE = 0x0003,
+
+            /// <summary>
+            /// Rename the object(s)
+            /// </summary>
+            FO_RENAME = 0x0004,
+        }
+
+        /// <summary>
+        /// SHFILEOPSTRUCT for SHFileOperation from COM
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SHFILEOPSTRUCT
+        {
+            public IntPtr hwnd;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public FileOperationType wFunc;
+
+            public string pFrom;
+            public string pTo;
+            public FileOperationFlags fFlags;
+
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool fAnyOperationsAborted;
+
+            public IntPtr hNameMappings;
+            public string lpszProgressTitle;
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern int SHFileOperation(ref SHFILEOPSTRUCT FileOp);
+
+        /// <summary>
+        /// Send file to recycle bin
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        /// <param name="flags">FileOperationFlags to add in addition to FOF_ALLOWUNDO</param>
+        public static bool Send(string path, FileOperationFlags flags)
+        {
+            try
+            {
+                var fs = new SHFILEOPSTRUCT
+                {
+                    wFunc = FileOperationType.FO_DELETE,
+                    pFrom = path + '\0' + '\0',
+                    fFlags = FileOperationFlags.FOF_ALLOWUNDO | flags
+                };
+                SHFileOperation(ref fs);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Send file to recycle bin.  Display dialog, display warning if files are too big to fit (FOF_WANTNUKEWARNING)
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        public static bool Send(string path)
+        {
+            return Send(path, FileOperationFlags.FOF_NOCONFIRMATION | FileOperationFlags.FOF_WANTNUKEWARNING);
+        }
+
+        /// <summary>
+        /// Send file silently to recycle bin.  Surpress dialog, surpress errors, delete if too large.
+        /// </summary>
+        /// <param name="path">Location of directory or file to recycle</param>
+        public static bool MoveToRecycleBin(string path)
+        {
+            return Send(path,
+                FileOperationFlags.FOF_NOCONFIRMATION |
+                FileOperationFlags.FOF_NOERRORUI |
+                FileOperationFlags.FOF_SILENT);
+        }
+
+        private static bool deleteFile(string path, FileOperationFlags flags)
+        {
+            try
+            {
+                var fs = new SHFILEOPSTRUCT
+                {
+                    wFunc = FileOperationType.FO_DELETE, pFrom = path + '\0' + '\0', fFlags = flags
+                };
+                SHFileOperation(ref fs);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteCompletelySilent(string path)
+        {
+            return deleteFile(path,
+                FileOperationFlags.FOF_NOCONFIRMATION |
+                FileOperationFlags.FOF_NOERRORUI |
+                FileOperationFlags.FOF_SILENT);
+        }
+    }
     // ReSharper restore all
 }

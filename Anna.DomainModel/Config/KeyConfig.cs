d3df91efb065cd1ac66b40fc465c0b0b2959c9de
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-26 07:27:10 +0900

    Refactor default KeyData order

--- Anna.DomainModel/Config/KeyConfig.cs
+++ Anna.DomainModel/Config/KeyConfig.cs
@@ -35,22 +35,29 @@ public sealed class KeyConfigData : ConfigData
         Keys = new KeyData[]
         {
             new(Key.S, KeyModifiers.None, Operations.SortEntry),
-            new(Key.J, KeyModifiers.None, Operations.JumpFolder),
+            //
             new(Key.Up, KeyModifiers.None, Operations.MoveCursorUp),
             new(Key.Down, KeyModifiers.None, Operations.MoveCursorDown),
             new(Key.Left, KeyModifiers.None, Operations.MoveCursorLeft),
             new(Key.Right, KeyModifiers.None, Operations.MoveCursorRight),
+            //
             new(Key.Space, KeyModifiers.None, Operations.ToggleSelectionCursorEntry),
-            new(Key.Enter, KeyModifiers.None, Operations.OpenEntry),
+            //
+            new(Key.J, KeyModifiers.None, Operations.JumpFolder),
             new(Key.Back, KeyModifiers.None, Operations.JumpToParentFolder),
             new(Key.OemPipe, KeyModifiers.None, Operations.JumpToRootFolder),
+            //
+            new(Key.Enter, KeyModifiers.None, Operations.OpenEntry),
             new(Key.Enter, KeyModifiers.Shift, Operations.OpenEntryByEditor1),
-            new(Key.Enter, metaKey, Operations.OpenEntryByApp), new(Key.C, KeyModifiers.None, Operations.CopyEntry),
+            new(Key.Enter, metaKey, Operations.OpenEntryByApp),
+            //
+            new(Key.C, KeyModifiers.None, Operations.CopyEntry),
             new(Key.D, KeyModifiers.None, Operations.DeleteEntry),
             new(Key.Delete, KeyModifiers.None, Operations.DeleteEntry),
+            new(Key.M, KeyModifiers.None, Operations.MoveEntry),
+            //
             new(Key.G, KeyModifiers.Shift, Operations.EmptyTrashCan),
             new(Key.G, KeyModifiers.Control, Operations.OpenTrashCan),
-            new(Key.M, KeyModifiers.None, Operations.MoveEntry),
         };
     }
 
@@ -61,21 +68,27 @@ public sealed class KeyConfigData : ConfigData
 public enum Operations
 {
     SortEntry,
-    JumpFolder,
+    //
     MoveCursorUp,
     MoveCursorDown,
     MoveCursorLeft,
     MoveCursorRight,
+    //
     ToggleSelectionCursorEntry,
+    //
+    JumpFolder,
+    JumpToParentFolder,
+    JumpToRootFolder,
+    //
     OpenEntry,
     OpenEntryByEditor1,
     OpenEntryByEditor2,
     OpenEntryByApp,
-    JumpToParentFolder,
-    JumpToRootFolder,
+    //
     CopyEntry,
     DeleteEntry,
+    MoveEntry,
+    //
     EmptyTrashCan,
     OpenTrashCan,
-    MoveEntry,
 }

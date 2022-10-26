77d37dae466b6418d2eda547800effed510b5e78
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-27 06:51:44 +0900

    Add ShellApiWrapper.cs

---
+++ Anna.Service.Windows/ShellApiWrapper.cs
@@ -0,0 +1,101 @@
+﻿// ReSharper disable all
+
+using System.Runtime.InteropServices;
+
+namespace Anna.Service.Windows;
+
+// https://stackoverflow.com/questions/3282418/send-a-file-to-the-recycle-bin 
+
+internal static class ShellApiWrapper
+{
+    public static bool EmptyTrashCan()
+    {
+        try
+        {
+            _ = SHEmptyRecycleBin(IntPtr.Zero,
+                null,
+                SHERB_NOCONFIRMATION | SHERB_NOPROGRESSUI | SHERB_NOSOUND);
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    public static bool SendToTrashCan(string path)
+    {
+        return Send(path,
+            FileOperationFlags.FOF_NOCONFIRMATION |
+            FileOperationFlags.FOF_NOERRORUI |
+            FileOperationFlags.FOF_SILENT);
+    }
+
+    [DllImport("Shell32.dll", CharSet = CharSet.Unicode)]
+    private static extern uint SHEmptyRecycleBin(IntPtr hwnd, string? pszRootPath, uint dwFlags);
+    
+    [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+    private static extern int SHFileOperation(ref SHFILEOPSTRUCT FileOp);
+    
+    private const uint SHERB_NOCONFIRMATION = 0x00000001;
+    private const uint SHERB_NOPROGRESSUI = 0x00000002;
+    private const uint SHERB_NOSOUND = 0x00000004;
+
+    [Flags]
+    private enum FileOperationFlags : ushort
+    {
+        FOF_SILENT = 0x0004,
+        FOF_NOCONFIRMATION = 0x0010,
+        FOF_ALLOWUNDO = 0x0040,
+        FOF_SIMPLEPROGRESS = 0x0100,
+        FOF_NOERRORUI = 0x0400,
+        FOF_WANTNUKEWARNING = 0x4000,
+    }
+
+    private enum FileOperationType : uint
+    {
+        FO_MOVE = 0x0001,
+        FO_COPY = 0x0002,
+        FO_DELETE = 0x0003,
+        FO_RENAME = 0x0004,
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    private struct SHFILEOPSTRUCT
+    {
+        public IntPtr hwnd;
+
+        [MarshalAs(UnmanagedType.U4)]
+        public FileOperationType wFunc;
+
+        public string pFrom;
+        public string pTo;
+        public FileOperationFlags fFlags;
+
+        [MarshalAs(UnmanagedType.Bool)]
+        public bool fAnyOperationsAborted;
+
+        public IntPtr hNameMappings;
+        public string lpszProgressTitle;
+    }
+
+    private static bool Send(string path, FileOperationFlags flags)
+    {
+        try
+        {
+            var fs = new SHFILEOPSTRUCT
+            {
+                wFunc = FileOperationType.FO_DELETE,
+                pFrom = path + '\0' + '\0',
+                fFlags = FileOperationFlags.FOF_ALLOWUNDO | flags
+            };
+            _ = SHFileOperation(ref fs);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}

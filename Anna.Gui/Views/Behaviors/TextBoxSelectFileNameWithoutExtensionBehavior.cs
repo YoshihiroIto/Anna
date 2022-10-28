f605b59fe5ae06c27dfb9116b33e20c8ec98e2e5
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-29 08:22:06 +0900

    Add TextBoxSelectFileNameWithoutExtensionBehavior

---
+++ Anna.Gui/Views/Behaviors/TextBoxSelectFileNameWithoutExtensionBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Xaml.Interactivity;
+
+namespace Anna.Gui.Views.Behaviors;
+
+public sealed class TextBoxSelectFileNameWithoutExtensionBehavior : Behavior<TextBox>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+
+        if (AssociatedObject is not null)
+            AssociatedObject.AttachedToVisualTree += AssociatedObjectOnAttachedToVisualTree;
+    }
+
+    private void AssociatedObjectOnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        if (AssociatedObject is null)
+            return;
+
+        AssociatedObject.AttachedToVisualTree -= AssociatedObjectOnAttachedToVisualTree;
+
+        var text = AssociatedObject.Text;
+        
+        if (text is null)
+            return;
+
+        var dotPos = text.LastIndexOf('.');
+
+        if (dotPos > 0)
+        {
+            AssociatedObject.SelectionStart = 0;
+            AssociatedObject.SelectionEnd = dotPos;
+        }
+        else
+            AssociatedObject.SelectAll();
+    }
+}

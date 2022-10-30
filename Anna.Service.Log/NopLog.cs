90805612512b3e5eefba9ecd9028b8857a461f86
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-30 18:21:54 +0900

    ILoggerService -> ILogService

---
+++ Anna.Service.Log/NopLog.cs
@@ -0,0 +1,14 @@
+﻿using Anna.Service.Services;
+
+namespace Anna.Service.Log;
+
+public sealed class NopLog : ILogService
+{
+    public void Destroy() {}
+    public void Verbose(string message) {}
+    public void Debug(string message) {}
+    public void Information(string message) {}
+    public void Warning(string message) {}
+    public void Error(string message) {}
+    public void Fatal(string message) {}
+}

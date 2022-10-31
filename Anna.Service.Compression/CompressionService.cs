ae5b83550132fbe5fc928473a1d5634c02ed0c53
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-10-31 18:23:54 +0900

    Reduced number of array rentals

--- Anna.Service.Compression/CompressionService.cs
+++ Anna.Service.Compression/CompressionService.cs
@@ -26,44 +26,50 @@ public sealed class CompressionService : ICompressionService
 
         using var folders = new TempBuffer<(string Path, DateTime TimeStamp)>(512);
 
-        // decompress files
-        using var inputFileStream = File.OpenRead(archiveFilePath);
-        using var zipInputStream = new ZipInputStream(inputFileStream);
+        var buffer = ArrayPool<byte>.Shared.Rent(64 * 1024);
 
-        var processed = 0;
-
-        while (zipInputStream.GetNextEntry() is {} entry)
+        try
         {
-            if (entry.IsDirectory)
+            // decompress files
+            using var inputFileStream = File.OpenRead(archiveFilePath);
+            using var zipInputStream = new ZipInputStream(inputFileStream);
+
+            var processed = 0;
+
+            while (zipInputStream.GetNextEntry() is {} entry)
             {
-                var folderPath = Path.GetDirectoryName(Path.Combine(destFolderPath, entry.Name)) ??
-                                 throw new NullReferenceException();
-
-                Directory.CreateDirectory(folderPath);
-
-                folders.Add((Path: folderPath, TimeStamp: entry.DateTime));
-            }
-            else
-            {
-                var buffer = ArrayPool<byte>.Shared.Rent((int)entry.Size);
-
-                try
+                if (entry.IsDirectory)
                 {
+                    var folderPath = Path.GetDirectoryName(Path.Combine(destFolderPath, entry.Name)) ??
+                                     throw new NullReferenceException();
+
+                    Directory.CreateDirectory(folderPath);
+
+                    folders.Add((Path: folderPath, TimeStamp: entry.DateTime));
+                }
+                else
+                {
+                    if (buffer.Length < entry.Size)
+                    {
+                        ArrayPool<byte>.Shared.Return(buffer);
+                        buffer = ArrayPool<byte>.Shared.Rent((int)entry.Size);
+                    }
+
                     _ = zipInputStream.Read(buffer, 0, buffer.Length);
 
                     var filePath = Path.Combine(destFolderPath, entry.Name);
                     FileSystemHelper.WriteSpan(filePath, buffer.AsSpan(0, (int)entry.Size));
                     File.SetLastWriteTimeUtc(filePath, entry.DateTime);
                 }
-                finally
-                {
-                    ArrayPool<byte>.Shared.Return(buffer);
-                }
+
+                ++processed;
+
+                onProgress((double)processed / fileCount);
             }
-
-            ++processed;
-
-            onProgress((double)processed / fileCount);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
         }
 
         // set folder timestamp

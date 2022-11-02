ef8771e61478b5e1aea709ef48825aa425b848ce
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-11-02 18:59:37 +0900

    Reduced boxing at ServiceProviderBase

--- Anna.ServiceProvider/ServiceProviderBase.cs
+++ Anna.ServiceProvider/ServiceProviderBase.cs
@@ -1,14 +1,12 @@
 ﻿using Anna.Service;
 using SimpleInjector;
-using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using IServiceProvider=Anna.Service.IServiceProvider;
 
 namespace Anna.ServiceProvider;
 
 public class ServiceProviderBase : Container, IServiceProvider
 {
-    private object? _argBox;
-
     public ServiceProviderBase()
     {
         RegisterSingleton<IServiceProvider>(() => this);
@@ -17,34 +15,37 @@ public class ServiceProviderBase : Container, IServiceProvider
         Options.EnableAutoVerification = false;
 #endif
     }
-    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public TService GetInstance<TService>(Type type) where TService : class
     {
         return (TService)GetInstance(type);
     }
-    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public THasArg GetInstance<THasArg, TArg>(TArg arg)
         where THasArg : class, IHasArg<TArg>
     {
         if (arg is null)
             throw new ArgumentNullException(nameof(arg));
 
-        Debug.Assert(_argBox is null);
+        ArgStore<TArg>.Arg = arg;
 
-        _argBox = arg;
-
-        var instance = GetInstance<THasArg>();
-
-        Debug.Assert(_argBox is null);
-
-        return instance;
+        return GetInstance<THasArg>();
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void PopArg<TArg>(out TArg arg)
     {
-        Debug.Assert(_argBox is not null);
+        arg = ArgStore<TArg>.Arg;
 
-        arg = (TArg)_argBox;
-        _argBox = null;
+        ArgStore<TArg>.Arg = default!;
+    }
+
+    private static class ArgStore<T>
+    {
+ #pragma warning disable CS8618
+        public static T Arg;
+ #pragma warning restore CS8618
     }
 }

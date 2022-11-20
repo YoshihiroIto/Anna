77ed7aef42acf40edf072bdfa0fd25296d392c86
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-11-20 18:47:16 +0900

    NotSupported ConvertBack

--- Anna.Gui/Views/Converters/BytesValueToHumanizedConverter.cs
+++ Anna.Gui/Views/Converters/BytesValueToHumanizedConverter.cs
@@ -23,6 +23,6 @@ public sealed class BytesValueToHumanizedConverter : IValueConverter
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
--- Anna.Gui/Views/Converters/EntrySizeToStringConverter.cs
+++ Anna.Gui/Views/Converters/EntrySizeToStringConverter.cs
@@ -81,6 +81,6 @@ public sealed class FolderEntrySizeToStringConverter : IValueConverter
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }

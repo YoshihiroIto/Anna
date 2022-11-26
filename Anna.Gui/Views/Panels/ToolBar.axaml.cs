ae33c6e1f4cf594521a40d1a973d07280a240427
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2022-11-27 08:54:48 +0900

    Add ToolBar

---
+++ Anna.Gui/Views/Panels/ToolBar.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Anna.Gui.Views.Panels;
+
+public sealed partial class ToolBar : DockPanel
+{
+    public ToolBar()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
